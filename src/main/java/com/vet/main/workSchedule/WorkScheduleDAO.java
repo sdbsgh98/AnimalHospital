0c1fef1877e4ff3e82cdf2bc4922ee4fa39851bf
Moment0129 <hrrr9400@gmail.com>
2023-11-16 16:11:14 +0900

    ggg

--- src/main/java/com/vet/main/workSchedule/WorkScheduleDAO.java
+++ src/main/java/com/vet/main/workSchedule/WorkScheduleDAO.java
@@ -1,23 +1,15 @@
 package com.vet.main.workSchedule;
 
-import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
-
-import com.vet.main.customer.CustomerVO;
-import com.vet.main.emp.EmpVO;
-import com.vet.main.reservation.surgery.SurgeryVO;
 
 @Mapper
 public interface WorkScheduleDAO {
 
-	public List<WorkScheduleVO> getWorkList() throws Exception;
 	
 	public int addWorkSchedule(WorkScheduleVO workScheduleVO) throws Exception;
 	
 	public int updateWorkSchedule(WorkScheduleVO workScheduleVO) throws Exception;
 
 	public WorkScheduleVO getWorkSchedule(WorkScheduleVO workScheduleVO) throws Exception;
-
-	public int delWorkSchedule(WorkScheduleVO workScheduleVO) throws Exception;
+	
 }
--- src/main/java/com/vet/main/workSchedule/WorkScheduleService.java
+++ src/main/java/com/vet/main/workSchedule/WorkScheduleService.java
@@ -1,7 +1,5 @@
 package com.vet.main.workSchedule;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,12 +27,5 @@ public class WorkScheduleService {
 	public WorkScheduleVO getWorkSchedule(WorkScheduleVO workScheduleVO) throws Exception {
 		return workScheduleDAO.getWorkSchedule(workScheduleVO);
 	}
-		
-	public List<WorkScheduleVO> getWorkList() throws Exception {
-		return workScheduleDAO.getWorkList();
-	}
 	
-	public int delWorkSchedule(WorkScheduleVO workScheduleVO) throws Exception {
-		return workScheduleDAO.delWorkSchedule(workScheduleVO);
-	}
 }
